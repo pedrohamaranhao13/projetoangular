af92c471945dcac8c77f8b93a2d3b9842fd7e301
pedrohamaranhao13 <pedrinhoelloco@outlook.com.br>
2025-02-26 15:05:34 -0300

    first commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { CriarUsuarioComponent } from './components/pages/criar-usuario/criar-usuario.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { LogoComponent } from './components/layout/logo/logo.component';
+import { RodapeComponent } from './components/layout/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    LoginComponent,
+    CriarUsuarioComponent,
+    LogoComponent,
+    RodapeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LogoComponent } from './components/layout/logo/logo.component';
+import { CriarUsuarioComponent } from './components/pages/criar-usuario/criar-usuario.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "app/login",
+        component: LoginComponent
+    },
+    {
+        path: "app/criar-usuario",
+        component: CriarUsuarioComponent
+    },
+    {
+        path: "app/index",
+        component: LogoComponent
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'app/index'
+    }
+];
